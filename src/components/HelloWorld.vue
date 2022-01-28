580b157fd6f2bf866f76e60c83037879b7d549cd
hashrock <hashrock@users.noreply.github.com>
2022-01-28 21:30:14 +0900

    use cropper

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,8 +8,8 @@ async function loadFromDir(parent, pattern, exclude) {
   }
   return files;
 }
-async function saveImageData(buffer, handle) {
-  const handleFile = await handle.getFileHandle("out.png", { create: true });
+async function saveImageData(buffer, handle, filename) {
+  const handleFile = await handle.getFileHandle(filename, { create: true });
   const writable = await handleFile.createWritable({
     // keepExistingData: false,
   });
@@ -19,12 +19,15 @@ async function saveImageData(buffer, handle) {
 import Cropper from "cropperjs";
 import "cropperjs/dist/cropper.css";
 
+
+let cropper = null
 export default {
   data() {
     return {
       msg: "HEllo",
       files: [],
       parent: null,
+      editing: null
     };
   },
   methods: {
@@ -35,6 +38,17 @@ export default {
       }
 
       this.files = await loadFromDir(this.parent, /.+\.png/, /.+\.thumb\.png/);
+    },
+    async loadCropper(f){
+      let reader = new FileReader();
+      reader.onload = async () => {
+        const _result = reader.result;
+        const image = new Image();
+        image.src = _result;
+        cropper.replace(_result)
+        this.editing = f
+      };
+      reader.readAsDataURL(await f.getFile());
     },
     async createThumb(f) {
       let reader = new FileReader();
@@ -58,11 +72,28 @@ export default {
       };
       reader.readAsDataURL(await f.getFile());
     },
+    crop(){
+      // 何か解像度が低い
+      // https://github.com/fengyuanchen/cropperjs/issues/372
+      cropper.getCroppedCanvas({
+        width: 300,
+        height: 300,
+        minWidth: 300,
+        minHeight: 300,
+        maxWidth: 4096,
+        maxHeight: 4096,
+        fillColor: '#fff',
+        imageSmoothingEnabled: true,
+        imageSmoothingQuality: 'high',
+      }).toBlob((blob)=>{
+        saveImageData(blob, this.parent, this.editing.name.replace(/\.png/, ".thumb.png"))
+      })
+    }
   },
   mounted() {
     const image = document.getElementById("image");
-    const cropper = new Cropper(image, {
-      aspectRatio: 16 / 9,
+    cropper = new Cropper(image, {
+      aspectRatio: 1,
       crop(event) {
         console.log(event.detail.x);
         console.log(event.detail.y);
@@ -80,9 +111,17 @@ export default {
 <template>
   <button @click="openDir">Open Dir</button>
   <div>
-    <img id="image" src="https://placekitten.com/g/200/300" />
+    <img id="image" />
+    <div>
+      <button @click="crop">Crop</button>
+    </div>
+  </div>
+
+  <div>
     <div v-for="(file, i) in files" :key="i">
-      {{ file.name }} <button @click="createThumb(file)">Create</button>
+      {{ file.name }} 
+      <button @click="createThumb(file)">Create</button>
+      <button @click="loadCropper(file)">Load</button>
     </div>
   </div>
 </template>
@@ -90,6 +129,11 @@ export default {
 <style scoped>
 a {
   color: #42b983;
+}
+img {
+  display: block;
+  max-width: 100%;
+  /* max-height: 400px; */
 }
 </style>
 
