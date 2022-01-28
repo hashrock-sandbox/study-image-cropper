288d674c8d99d46dbcdef4b204cf3a925ada7fd2
hashrock <hashrock@users.noreply.github.com>
2022-01-28 20:41:47 +0900

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,6 @@
 <script>
-async function loadFromDir(pattern, exclude) {
+async function loadFromDir(parent, pattern, exclude) {
   const files = [];
-  const parent = await window.showDirectoryPicker();
-  if (!parent) {
-    return;
-  }
   for await (let handle of parent.values()) {
     if (!handle.name.match(exclude) && handle.name.match(pattern)) {
       files.push(handle);
@@ -12,17 +8,51 @@ async function loadFromDir(pattern, exclude) {
   }
   return files;
 }
+async function saveImageData(buffer, handle) {
+  const handleFile = await handle.getFileHandle("out.png", { create: true });
+  const writable = await handleFile.createWritable({
+    // keepExistingData: false,
+  });
+  await writable.write(buffer);
+  await writable.close();
+}
 
 export default {
   data() {
     return {
       msg: "HEllo",
       files: [],
+      parent: null,
     };
   },
   methods: {
     async openDir() {
-      this.files = await loadFromDir(/.+\.png/, /.+\.thumb\.png/);
+      this.parent = await window.showDirectoryPicker();
+      if (!this.parent) {
+        return;
+      }
+
+      this.files = await loadFromDir(this.parent, /.+\.png/, /.+\.thumb\.png/);
+    },
+    async createThumb(f) {
+      let reader = new FileReader();
+      reader.onload = async () => {
+        const _result = reader.result;
+        const image = new Image();
+        image.src = _result;
+        const canvas = document.createElement("canvas");
+        canvas.width = 100;
+        canvas.height = 100;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(image, 0, 0, 100, 100);
+        document.body.appendChild(canvas)
+        // var imageData = ctx.getImageData(0, 0, 100, 100);
+        // var buffer = imageData.data.buffer;
+        canvas.toBlob((blob)=>{
+          saveImageData(blob, this.parent);
+        })
+      };
+      reader.readAsDataURL(await f.getFile());
     },
   },
 };
@@ -32,7 +62,7 @@ export default {
   <button @click="openDir">Open Dir</button>
   <div>
     <div v-for="(file, i) in files" :key="i">
-      {{ file.name }}
+      {{ file.name }} <button @click="createThumb(file)">Create</button>
     </div>
   </div>
 </template>
@@ -42,3 +72,4 @@ a {
   color: #42b983;
 }
 </style>
+
