ff479a1a0d76e25a97d33e45c67598032708e5be
hashrock <hashrock@users.noreply.github.com>
2022-01-28 21:51:36 +0900

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,35 @@
+<template>
+  <button @click="openDir">Open Dir</button>
+  <img id="image" />
+  <div v-if="editing">
+    <div>
+      <button @click="crop">Save Thumbnail</button>
+    </div>
+  </div>
+
+  <div>
+    <div class="item" v-for="(file, i) in files" :key="i" @click="loadCropper(file)">
+      {{ file.name }} 
+      <span v-if="hasThumbnail.includes(file.name)">✔</span>
+    </div>
+  </div>
+</template>
+
 <script>
+import Cropper from "cropperjs";
+import "cropperjs/dist/cropper.css";
+
 async function loadFromDir(parent, pattern, exclude) {
   const files = [];
   for await (let handle of parent.values()) {
-    if (!handle.name.match(exclude) && handle.name.match(pattern)) {
-      files.push(handle);
+    if(exclude){
+      if (!handle.name.match(exclude) && handle.name.match(pattern)) {
+        files.push(handle);
+      }
+    }else{
+      if (handle.name.match(pattern)) {
+        files.push(handle);
+      }
     }
   }
   return files;
@@ -16,28 +42,28 @@ async function saveImageData(buffer, handle, filename) {
   await writable.write(buffer);
   await writable.close();
 }
-import Cropper from "cropperjs";
-import "cropperjs/dist/cropper.css";
-
 
 let cropper = null
 export default {
   data() {
     return {
-      msg: "HEllo",
       files: [],
       parent: null,
-      editing: null
+      editing: null,
+      thumbs: []
     };
   },
   methods: {
+    async findThumbs(){
+      this.thumbs = await loadFromDir(this.parent, /.+\.thumb\.png/);
+    },
     async openDir() {
       this.parent = await window.showDirectoryPicker();
       if (!this.parent) {
         return;
       }
-
       this.files = await loadFromDir(this.parent, /.+\.png/, /.+\.thumb\.png/);
+      this.findThumbs()
     },
     async loadCropper(f){
       let reader = new FileReader();
@@ -47,28 +73,6 @@ export default {
         image.src = _result;
         cropper.replace(_result)
         this.editing = f
-      };
-      reader.readAsDataURL(await f.getFile());
-    },
-    async createThumb(f) {
-      let reader = new FileReader();
-      reader.onload = async () => {
-        const _result = reader.result;
-        const image = new Image();
-        image.src = _result;
-        const canvas = new OffscreenCanvas(100, 100);
-        // canvas.width = 100;
-        // canvas.height = 100;
-        const ctx = canvas.getContext("2d");
-        ctx.imageSmoothingEnabled = true;
-        // ctx.translate(0.5, 0.5);
-        ctx.drawImage(image, 0, 0, 100, 100);
-        // document.body.appendChild(canvas)
-        // var imageData = ctx.getImageData(0, 0, 100, 100);
-        // var buffer = imageData.data.buffer;
-        const blob = await canvas.convertToBlob();
-        console.log(blob);
-        saveImageData(blob, this.parent);
       };
       reader.readAsDataURL(await f.getFile());
     },
@@ -85,9 +89,15 @@ export default {
         fillColor: '#fff',
         imageSmoothingEnabled: true,
         imageSmoothingQuality: 'high',
-      }).toBlob((blob)=>{
-        saveImageData(blob, this.parent, this.editing.name.replace(/\.png/, ".thumb.png"))
+      }).toBlob(async (blob)=>{
+        await saveImageData(blob, this.parent, this.editing.name.replace(/\.png/, ".thumb.png"))
+        await this.findThumbs()
       })
+    }
+  },
+  computed: {
+    hasThumbnail(){
+      return this.thumbs.map(i => i.name.replace(".thumb.png", ".png"))
     }
   },
   mounted() {
@@ -95,45 +105,26 @@ export default {
     cropper = new Cropper(image, {
       aspectRatio: 1,
       crop(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
       },
     });
   },
 };
 </script>
 
-<template>
-  <button @click="openDir">Open Dir</button>
-  <div>
-    <img id="image" />
-    <div>
-      <button @click="crop">Crop</button>
-    </div>
-  </div>
-
-  <div>
-    <div v-for="(file, i) in files" :key="i">
-      {{ file.name }} 
-      <button @click="createThumb(file)">Create</button>
-      <button @click="loadCropper(file)">Load</button>
-    </div>
-  </div>
-</template>
-
 <style scoped>
+.item{
+  cursor: pointer;
+}
+.item:hover{
+  background: #AAA;
+}
 a {
   color: #42b983;
 }
 img {
   display: block;
   max-width: 100%;
-  /* max-height: 400px; */
+  max-height: 400px;
 }
 </style>
 
