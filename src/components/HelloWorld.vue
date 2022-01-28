eaf44e9e3827c07d399803b552751f5c49514e64
hashrock <hashrock@users.noreply.github.com>
2022-01-28 21:01:39 +0900

    add cropperjs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,8 @@ async function saveImageData(buffer, handle) {
   await writable.write(buffer);
   await writable.close();
 }
+import Cropper from "cropperjs";
+import "cropperjs/dist/cropper.css";
 
 export default {
   data() {
@@ -40,21 +42,37 @@ export default {
         const _result = reader.result;
         const image = new Image();
         image.src = _result;
-        const canvas = new OffscreenCanvas(100, 100)
+        const canvas = new OffscreenCanvas(100, 100);
         // canvas.width = 100;
         // canvas.height = 100;
         const ctx = canvas.getContext("2d");
         ctx.imageSmoothingEnabled = true;
+        // ctx.translate(0.5, 0.5);
         ctx.drawImage(image, 0, 0, 100, 100);
         // document.body.appendChild(canvas)
         // var imageData = ctx.getImageData(0, 0, 100, 100);
         // var buffer = imageData.data.buffer;
-        const blob = await canvas.convertToBlob()
-        console.log(blob)
+        const blob = await canvas.convertToBlob();
+        console.log(blob);
         saveImageData(blob, this.parent);
       };
       reader.readAsDataURL(await f.getFile());
     },
+  },
+  mounted() {
+    const image = document.getElementById("image");
+    const cropper = new Cropper(image, {
+      aspectRatio: 16 / 9,
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      },
+    });
   },
 };
 </script>
@@ -62,6 +80,7 @@ export default {
 <template>
   <button @click="openDir">Open Dir</button>
   <div>
+    <img id="image" src="https://placekitten.com/g/200/300" />
     <div v-for="(file, i) in files" :key="i">
       {{ file.name }} <button @click="createThumb(file)">Create</button>
     </div>
